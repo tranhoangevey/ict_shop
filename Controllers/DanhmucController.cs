d614e1e000283bfc2952abf184266ddd4195cca0
Evey <128909652+tranhoangevey@users.noreply.github.com>
2023-03-26 10:16:17 +0700

    Add files via upload

---
+++ Controllers/DanhmucController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class DanhmucController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // GET: Danhmuc
+        public ActionResult DanhmucPartial()
+        {
+            return PartialView(danhmuc);
+        }
+    }
+}
